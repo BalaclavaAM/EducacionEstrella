9564ee83a60e1b9d0ecefaa7037141f518d7ceb2
Juan Camacho <68751794+BalaclavaAM@users.noreply.github.com>
2022-05-13 22:38:05 -0500

    see description
    - Added JWT verification
    - Added private endpoints for finance
    - Added logs for redudance for log

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import json
+import os
 import waitress
 import restApi
 
+
+
 if __name__=="__main__":
-    print("Starting server...")
-    waitress.serve(restApi.app, host='0.0.0.0', port=5000)
+    with open('config.json') as json_file:
+        data = json.load(json_file)
+        os.environ['AUTH0_CLIENT_ID'] = data['auth0']['client_id']
+        os.environ['AUTH0_CLIENT_SECRET'] = data['auth0']['client_secret']
+        os.environ['AUTH0_DOMAIN'] = data['auth0']['domain']
+    print("Starting server... in port 8080")
+    waitress.serve(restApi.app, host='0.0.0.0', port=8080)
